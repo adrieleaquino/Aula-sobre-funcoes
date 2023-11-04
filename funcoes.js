45e4e536e28d5524bcbf49c9646f868bb13ef6dd
ADRIELE OLIVEIRA DE AQUINO <ADRIELE0471@DIGITALCOLLEGE.LOCAL>
2023-11-04 16:27:59 -0300

    js-funções

---
+++ funcoes.js
@@ -0,0 +1,56 @@
+//function miar() {
+    //let texto = "miau";
+    //alert(texto);
+//}
+//miar()
+
+//function nome() {
+    //let texto = "Adriele"; 
+    //alert(texto);
+//}
+//nome()
+
+//function chamarAdriele() { 
+    //let texto = "Adriele";
+    //alert(texto);
+//}//quando chama no html, não precisa chamar no JS.
+
+//function qualseunome() {
+    //let nome = prompt("Qual seu nome?");
+    //alert(nome);
+//}
+//qualseunome()
+
+//function dizUmNome() {
+    //let nome = "Ronier"
+    //alert("Comecei a função")
+    //return "teste";
+//}
+
+//let retorn = dizUmNome()
+
+//alert(retorn)
+
+//function pi() {
+    //return 3.14;
+//}
+//let retornar = pi() * 2;
+
+//function quadrado(lado) {
+    //alert(lado);
+    //return lado * lado;
+//}
+
+// retorno = quadrado(2);
+//alert(retorno)
+
+//function multi5(numero) {
+    //return numero * 5;
+//}
+ //alert(multi5(10))
+
+ function soma(numero1, numero2) {
+    return numero1 + numero2;
+ }
+
+ alert(soma(4, 5))
